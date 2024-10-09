b27846a1ef09b9688c3a525347d3e2368f6b1bcf
Han <puhoi125@mil.ru>
2024-10-09 18:33:56 +0300

    Initial commit home reset

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-index_number = -1
-len_my_list = len(my_list)
-while (index_number + 1 < len_my_list):
-    index_number += 1
-    if my_list[index_number] == 0:
-        continue
-    if my_list[index_number] > 0:
-        print(my_list[index_number])
-    else:
-        break
+my_dict = {'lavra': 12457890, 'troi':22366745, 'nastia': 2143567809}
+print(my_dict)
+print(my_dict['troi'])
+my_dict['nastia'] = 33456633289
+my_dict['anton'] = 895477643321
+print(my_dict)
+my_dict.update({'max': 33442211 ,'lola':45890889068})
+del my_dict['lavra']
+print(my_dict)
+
+# Множества
+my_set = {1, 2, 3, 2, 4, 4, 5, True, 'list', 'map'}
+my_set.add('hope'),
+my_set.add(8)
+print('Множество:', my_set)
+my_set.discard('list')
+print('Измененное множество:', my_set)
+
